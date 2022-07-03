284b44c65500ebefa58eacd7352de5f5e0afa5b7
howard <howard@jinnstudios.com>
2022-07-03 16:15:34 -0500

    Initial commit.

---
+++ DeviceTimer/DeviceTimerApp.groovy
@@ -0,0 +1,128 @@
+definition(
+    name: "Device Timer App",
+    namespace: "masterHman",
+    author: "Howard Roberson",
+    description: "Automatically resets devices to preferred on/off setting after specified amount of time",
+    importUrl: "",
+    iconUrl: "",
+    iconX2Url: "",
+    singleInstance: true
+)
+
+preferences {
+     page name: "mainPage", title: "", install: true, uninstall: true
+} 
+
+def installed() {
+    logDebug("Installed with settings: ${settings}")
+    initialize()
+}
+
+def updated() {
+    logDebug("Updated with settings: ${settings}")
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    logDebug( "There are ${childApps.size()} child apps")
+    childApps.each {child ->
+        logDebug( "Child app: ${child.label}")
+    }
+}
+
+def mainPage() {
+    dynamicPage(name: "mainPage") {
+        installCheck()
+        if(state.appInstalled == 'COMPLETE'){
+            section("Instructions:", hideable: true, hidden: true) {
+                paragraph "<b>Information</b>"
+                paragraph "Simple way to monitor if your hub is slowing down or not."
+            }
+            section(getFormat("header-green", "${getImage("Blank")}"+" Child Apps")) {
+                app(name: "anyOpenApp", appName: "Hub Watchdog Child", namespace: "BPTWorld", title: "<b>Add a new 'Hub Watchdog' child</b>", multiple: true)
+                app(name: "anyOpenApp", appName: "Hub Watchdog Examiner Child", namespace: "BPTWorld", title: "<b>Add a new 'Hub Watchdog Examiner' child</b>", multiple: true)
+            }
+            
+            section(getFormat("header-green", "${getImage("Blank")}"+" General")) {
+                label title: "Enter a name for parent app (optional)", required: false
+            }
+            display2()
+        }
+    }
+}
+
+def installCheck(){
+    display()
+    state.appInstalled = app.getInstallationState() 
+    if(state.appInstalled != 'COMPLETE'){
+        section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
+    }
+    else{
+        logDebug( "Parent Installed OK")
+    }
+}
+
+def getImage(type) {					// Modified from @Stephack Code
+    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
+    if(type == "Blank") return "${loc}blank.png height=40 width=5}>"
+    if(type == "checkMarkGreen") return "${loc}checkMarkGreen2.png height=30 width=30>"
+    if(type == "optionsGreen") return "${loc}options-green.png height=30 width=30>"
+    if(type == "optionsRed") return "${loc}options-red.png height=30 width=30>"
+    if(type == "instructions") return "${loc}instructions.png height=30 width=30>"
+    if(type == "logo") return "${loc}logo.png height=60>"
+}
+
+def getFormat(type, myText="") {			// Modified from @Stephack Code   
+    if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
+    if(type == "line") return "<hr style='background-color:#1A77C9; height: 1px; border: 0;'>"
+    if(type == "title") return "<h2 style='color:#1A77C9;font-weight: bold'>${myText}</h2>"
+}
+
+def display() {
+    setVersion()
+    getHeaderAndFooter()
+    theName = app.label
+    if(theName == null || theName == "") theName = "New Child App"
+    section (getFormat("title", "${getImage("logo")}" + " ${state.name} - ${theName}")) {
+        paragraph "${state.headerMessage}"
+        paragraph getFormat("line")
+    }
+}
+
+def display2() {
+    section() {
+        paragraph getFormat("line")
+        paragraph "<div style='color:#1A77C9;text-align:center;font-size:20px;font-weight:bold'>${state.name} - ${state.version}</div>"
+        paragraph "${state.footerMessage}"
+    }       
+}
+
+def getHeaderAndFooter() {
+    if(logEnable) logDebug "In getHeaderAndFooter (${state.version})"
+    def params = [
+        uri: "https://raw.githubusercontent.com/bptworld/Hubitat/master/info.json",
+        requestContentType: "application/json",
+        contentType: "application/json",
+        timeout: 30
+    ]
+    
+    try {
+        def result = null
+        httpGet(params) { resp ->
+            state.headerMessage = resp.data.headerMessage
+            state.footerMessage = resp.data.footerMessage
+        }
+        if(logEnable) logDebug "In getHeaderAndFooter - headerMessage: ${state.headerMessage}"
+        if(logEnable) logDebug "In getHeaderAndFooter - footerMessage: ${state.footerMessage}"
+    }
+    catch (e) {
+        state.headerMessage = "<div style='color:#1A77C9'><a href='https://github.com/bptworld/Hubitat' target='_blank'>BPTWorld Apps and Drivers</a></div>"
+        state.footerMessage = "<div style='color:#1A77C9;text-align:center'>BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br><a href='https://paypal.me/bptworld' target='_blank'>Paypal</a></div>"
+    }
+}
+
+private logDebug(msg) {
+    if (debugEnable) 
+        log.debug(msg)
+}
---
+++ DeviceTimer/DeviceTimerConfig.groovy
@@ -0,0 +1,114 @@
+
+import java.text.SimpleDateFormat
+
+definition(
+    name: "Device Timer Configuration",
+    namespace: "masterHman",
+    author: "Howard Roberson",
+    description: "Automatically resets devices to preferred on/off setting after specified amount of time",
+    importUrl: "",
+    iconUrl: "",
+    iconX2Url: "",
+    singleInstance: false
+)
+
+preferences {
+    page name: "mainPage", title: "Timer Configuration", install: true, uninstall: true
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    unschedule()
+    initialize()
+}
+
+private initialize() {
+    logDebug("Initialize with settings: ${settings}")
+    state.deviceList = [:]
+
+    subscribe(devices, "switch", onDeviceToggle)
+    runEvery1Minute(scheduleHandler)
+}
+
+def mainPage() {
+    dynamicPage(name: "mainPage") {
+        section() {
+            label(title: "Name", required: false)
+            input(name: "devices", type: "capability.switch", title: "When one of these devices:", required: true, multiple: true)        
+            input(name: "whenDeviceIsTurnedOn", type: "bool", title: "Is turned <b>" + ((whenDeviceIsTurnedOn == true) ? "on</b>" : "off</b>"), defaultValue: false, submitOnChange:true)  
+            input(name: "timerValue", type: "number", title: "Wait for...(in minutes)", required: true)            
+            paragraph("And turn it back <b>" + ((whenDeviceIsTurnedOn == true) ? "off</b>" : "on</b>")) 
+            input(name: "master", type: "capability.switch", title: "But, only if this Switch:", multiple: false, submitOnChange: true)
+            if(master){
+                input(name: "isMasterOn", type: "bool", title: "Is turned <b>" + ((isMasterOn == true) ? "on</b>" : "off</b>"), defaultValue: false, submitOnChange:true)            
+            }          
+        }
+        section() {
+            input(name: "isInfoLogging", type: "bool", defaultValue: "true", title: "Enable Info (descriptionText) Logging")  
+            input(name: "isDebugLogging", type: "bool", defaultValue: "false", title: "Enable Debug Logging")            
+        }
+    }
+}
+
+/**
+Event handler called when any of the devices are toggled.
+**/
+def onDeviceToggle(evt) {
+    logDeviceToggle(evt)
+
+    if(evt.value == "on" && whenDeviceIsTurnedOn == true){
+        logDebug("Device id:[${evt.device.id}] was turned [${evt.value}]. Adding to device list.")
+        state.deviceList[evt.device.id] = getTimeOutValue()
+        return        
+    }else if(evt.value == "off" && whenDeviceIsTurnedOn == false){   
+        logDebug("Device id:[${evt.device.id}] was turned [${evt.value}]. Adding to device list.")     
+        state.deviceList[evt.device.id] = getTimeOutValue()
+        return    
+    }
+    
+    logDebug("Device id:[${evt.device.id}] was turned [${evt.value}]. Removing from device list.") 
+    state.deviceList.remove(evt.device.id)
+}
+
+def scheduleHandler() {
+    // Find all map entries with an off-time that is earlier than now
+    def actionList = state.deviceList.findAll { it.value < now() }
+
+    // Find all devices that match the off-entries from above
+    def deviceList = devices.findAll { device -> actionList.any { it.key == device.id } }
+
+    logDebug ("scheduleHandler now:${now()} offList:${state.deviceList} actionList:${actionList} deviceList:${deviceList}")
+
+    // Call off(), or on() if inverted, on all relevant devices and remove them from offList
+    if (!master || master.latestValue("switch") == "on") {
+        if (whenDeviceIsTurnedOn) deviceList*.on()
+        else deviceList*.off()
+    } else {
+        logDebug "Skipping actions because MasterSwitch '${master?.displayName}' is Off"
+    }
+    state.deviceList -= actionList
+}
+
+private logDebug(msg) {
+    if (debugEnable) 
+        log.debug(msg)
+}
+
+private logInfo(msg) {
+    if (infoEnabled) 
+        log.debug(msg)
+}
+
+private logDeviceToggle(evt){    
+    logInfo("onDeviceToggle -> Device:[${evt.device}] was turned [${evt.value}] will start timer if the device was turned [" + ((whenDeviceIsTurnedOn == true) ? "on" : "off") + "]")       
+    logDebug("onDeviceToggle -> now:${now()} evt.device:${evt.device}, evt.value:${evt.value}, state:${state}, " +
+        "${evt.value == "on"} ^ ${whenDeviceIsTurnedOn==true} = ${(evt.value == "on") ^ (whenDeviceIsTurnedOn == true)}")
+}
+
+private int getTimeOutValue(){
+    return now() + timerValue * 60 * 1000
+}
