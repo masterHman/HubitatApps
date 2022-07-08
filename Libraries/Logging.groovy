fed94b588bf3c1d659a955b078541d5346649add
howard <howard@jinnstudios.com>
2022-07-07 22:46:09 -0500

    Added the logging methods.

--- Libraries/Logging.groovy
+++ Libraries/Logging.groovy
@@ -6,3 +6,20 @@ library (
  namespace: "TrevTelSolutions",
  documentationLink: "http://www.example.com/"
 )
+
+def addLoggingSection(){
+    section("Logging:", hideable: true, hidden: true) {
+        input(name: "isInfoLogging", type: "bool", defaultValue: "true", title: "Enable Info (descriptionText) Logging")  
+        input(name: "isDebugLogging", type: "bool", defaultValue: "false", title: "Enable Debug Logging")            
+    }
+}
+
+private logDebug(msg) {
+    if (isDebugLogging) 
+        log.debug(msg)
+}
+
+private logInfo(msg) {
+    if (isInfoLogging) 
+        log.debug(msg)
+}
