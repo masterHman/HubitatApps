78196c5c6bfa2e4ba68b84636ad17daa81b48b89
howard <howard@jinnstudios.com>
2022-07-07 22:45:18 -0500

    Added common library.

---
+++ Libraries/Common.groovy
@@ -0,0 +1,38 @@
+library (
+ author: "masterHman",
+ category: "utility",
+ description: "Common methods.",
+ name: "Common",
+ namespace: "TrevTelSolutions",
+ documentationLink: "http://www.example.com/"
+)
+
+
+def getAppInfoFromUri(uri){
+    def params = [
+        uri: uri,
+        requestContentType: "application/json",
+        contentType: "application/json",
+        timeout: 30
+    ]
+    
+    try {
+        def result = null
+        httpGet(params)
+        {
+            resp ->
+            state.appInfo = 
+            [
+            name: resp.data.name,
+            version: resp.data.version,
+            namespace: resp.data.namespace,
+            title: resp.data.title,
+            description: resp.data.description,
+            childApp = [label: resp.data.childApp.label]
+            ]
+        }
+    }
+    catch (e) {
+        logDebug("Exception in loadSettings: ${e}")       
+    }
+}
